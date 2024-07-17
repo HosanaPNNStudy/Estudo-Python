16070c9027df258cab0c2c0df47d75820847b439
Programação em Python 2024.4 <36129532024.4@senacrjedu.local>
2024-07-17 12:01:58 -0300

    Primeiro Commit

---
+++ a4.py
@@ -0,0 +1,9 @@
+maiorIdade = 18
+idade = int(input('Digite a idade\n'))
+
+
+if(idade >= maiorIdade):
+    print('Eh maior de idade')
+
+else:
+    print ('Eh menor de idade')
---
+++ a5.js
@@ -0,0 +1,29 @@
+// let numero1 = 1
+// let numero2 = 2
+// let numero3 = 3
+
+// if (numero1 >= numero2 && numero1 >= numero3){
+//     console.log(numero1)
+// }
+// else if ( numero2 > numero3 ){
+//     console.log(numero2)
+// }
+// else {
+//     console.log(numero3)
+// }
+
+let nota1 = 5.5
+let nota2 = 4
+let nota3 = 2
+
+media = (nota1 + nota2 + nota3)/3
+
+if (media <= 7 && media >= 5) {
+    console.log("Aluno em Recuperacao")
+}
+else if ( media > 7){
+    console.log("Aluno Aprovado")
+}
+else{
+    console.log("Aluno Reprovado")
+}
