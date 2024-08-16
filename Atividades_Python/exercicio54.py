182fad5ccb50b0b625ab3f63bf06ea34f0f21dc7
Programação em Python 2024.4 <36129532024.4@senacrjedu.local>
2024-08-16 11:10:36 -0300

    Atividades finalizadas

--- Atividades_Python/exercicio54.py
+++ Atividades_Python/exercicio54.py
@@ -5,11 +5,11 @@ for i in range(0,2):
     salario = input('Digite seu salario: ')
     professor = input('Você professor? s ou n: ')
     
-
+    
     if professor == 's':
         disciplina = input('Escreva a disciplina que leciona: ')
-        funcionarios.append('Nome: ', nome , ' Salario: ', salario , ' Disciplina: ' , disciplina + '.\n' )
+        funcionarios.append('Nome: '+ nome + ' Salario: '+ salario + ' Disciplina: ' + disciplina + '.\n' )
     else:
-        funcionarios.append('Nome: ', nome ,' Salario: ',salario + '.\n' )
+        funcionarios.append('Nome: '+ nome + ' Salario: '+ salario + '.\n' )
 
 print(funcionarios)
